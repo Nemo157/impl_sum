4f3dbfb628830faa8f4787601391a71b90684169
Wim Looman <wim@nemo157.com>
2018-05-03 21:00:06 +0200

    Move impls to separate modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![feature(use_extern_macros)]
 extern crate impl_sum_macro;
 
+mod impls;
+
 pub use impl_sum_macro::impl_sum;
 
 macro_rules! enums {
@@ -16,28 +18,5 @@ macro_rules! enums {
 }
 
 enums! {
-    Z Y X W V U T S R Q P O N M L K J I H G F E D C B A
-}
-
-macro_rules! impl_iterator {
-    ($head:ident) => {
-    };
-
-    ($head:ident $head2:ident $($rest:ident)*) => {
-        impl<$head2, $($rest),*> Iterator for $head<$head2, $($rest),*> where $head2: Iterator, $($rest: Iterator<Item = $head2::Item>),* {
-            type Item = $head2::Item;
-
-            fn next(&mut self) -> Option<Self::Item> {
-                match *self {
-                    $head::$head2(ref mut e) => e.next(),
-                    $($head::$rest(ref mut e) => e.next(),)*
-                }
-            }
-        }
-        impl_iterator!($head2 $($rest)*);
-    };
-}
-
-impl_iterator! {
     Z Y X W V U T S R Q P O N M L K J I H G F E D C B A
 }
